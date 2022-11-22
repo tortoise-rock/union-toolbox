d231e80b1460b08271218ee304d425da68ccb04d
Rob Mosher <rob@robmosher.com>
2022-11-21 21:58:44 -0500

    React functional (for real this time)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/app.jsx',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
