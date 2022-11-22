b9d639ae69073bbc1f7ae63c6ee308dfb1c0edc1
Rob Mosher <rob@robmosher.com>
2022-11-22 00:33:17 -0500

    Redux skeleton (#1)
    * React functional
    
    * Redux functional

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Union Toolbox Frontend</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
