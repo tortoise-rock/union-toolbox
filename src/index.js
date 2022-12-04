e0582d333f7de08d127f7138fe5e96f53f8f2d6b
Rob Mosher <rob@robmosher.com>
2022-12-04 18:49:03 -0500

    Updated default error status code

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use('*', (req, res, next) => {
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
-    status: 400,
+    status: 500,
     message: { err: 'An error occurred' },
   };
 
