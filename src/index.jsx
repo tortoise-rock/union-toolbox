425fa1ae90457a5a0709f9c08367f63ba57b1aa4
Rob Mosher <rob@robmosher.com>
2022-12-14 04:38:10 -0500

    Added rudimentary routing (#4)

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
+import ErrorPage from './error-page';
 import store from './app/store';
 import App from './app/App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
