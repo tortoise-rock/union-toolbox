f4010958ed0fe981569307016b32f2a72bffefb2
Rob Mosher <rob@robmosher.com>
2022-12-16 00:08:02 -0500

    Import patch from backend repo (#6)

---
+++ src/api/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+const { PORT } = process.env;
+
+app.use(express.json());
+
+const apiRouter = require('./routes/api');
+
+app.use('/api', apiRouter);
+
+app.use('*', (req, res, next) => {
+  const errorObj = {
+    log: 'Page not found',
+    status: 404,
+    message: { err: 'Error 404: Page not Found' },
+  };
+  next(errorObj);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+
+  res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
