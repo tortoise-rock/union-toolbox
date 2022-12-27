bcde58f76e7e887c58718eb075cf8d9f9bc78031
Rob Mosher <rob@robmosher.com>
2022-12-27 17:57:20 -0500

    Adjusted header visuals

--- src/web/common/components/Header.jsx
+++ src/web/common/components/Header.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 function Header() {
   return (
     <div id='header'>
-      <div id='sub-header' className='ui fixed top sticky fluid container'>
+      <div id='sub-header' className='ui fixed top sticky fluid container' style={{ padding: '6px 0' }}>
         {/* LEFT */}
         {/* END LEFT */}
         {/* CENTER */}
         <div className='ui menu secondary'>
-          <a className='item' href='#todo'>
+          <a className='ui item' href='#todo'>
             <i className='layer group icon' />
             <span className='tool-name'>PAYROLL</span>
           </a>
@@ -19,12 +19,12 @@ function Header() {
             Tools
             <i className='dropdown icon' />
             <div className='menu'>
-              <a className='item' href='#todo'>
+              <a className='ui item' href='#todo'>
                 Member Lookup
               </a>
             </div>
           </div>
-          <div className='item'>
+          <div className='ui item'>
             <a className='ui button primary' href='#todo'>
               Upload
             </a>
@@ -32,8 +32,8 @@ function Header() {
           {/* END CENTER */}
           {/* RIGHT */}
           <div className='right menu'>
-            <div className='item'>
-              <form className='ui form'>
+            <div className='ui item' style={{ padding: 0 }}>
+              <form className='ui form' style={{ margin: 0, padding: '0px 8px' }}>
                 <div className='ui search'>
                   <div className='ui icon input'>
                     <input
@@ -47,10 +47,10 @@ function Header() {
                   <div className='results' />
                 </div>
               </form>
-              <a href='#todo' className='ui item'>
+              <a href='#todo' className='ui item' style={{ margin: 0, padding: '0px 8px' }}>
                 <i className='cog icon' />
               </a>
-              <a href='#todo' className='ui item'>
+              <a href='#todo' className='ui item' style={{ margin: 0, padding: '0px 8px' }}>
                 <i className='circular user icon' />
               </a>
             </div>
